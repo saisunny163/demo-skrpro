a5da4600ebabde0024bb2bb8f02f8340919c1d58
saikumar <saikumarsunny709@gmail.com>
2022-01-26 17:12:56 +0530

    added intial commit

---
+++ src/main/java/com/skcompany/demo_skrpro/App.java
@@ -0,0 +1,13 @@
+package com.skcompany.demo_skrpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
